e3d635e06761ba0475d1aa252e9cd8f1a6545a4a
Luis Desktop <lmgspain@hotmail.com>
2022-08-24 00:48:02 +0200

    Añade funcion total personas por mes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-    function get_db(){//rescata las variables de BBDD
+    function get_db(){//rescata las variables de BBDD del fichero
         $fichero = fopen("config.ini", "r");
         $DB_Vars = array();
         while (!feof($fichero)){ 
@@ -28,7 +28,7 @@
         }
     }
 
-    function get_entradas_by_date($date){//lanzará una query que obtenga todas las entradas solicitadas por parámetro
+    function get_entradas_by_date($date){//lanzará una query que obtenga todas las entradas solicitadas por parámetro de determinada fecha
         $sql = "SELECT * FROM `reservas` WHERE `fecha_entrada` ='".$date."'";
         $enlace = conectar();
         $result = $enlace->query($sql);
@@ -36,7 +36,7 @@
         return $rows;
     }
 
-    function get_estancias_by_interval($date1, $date2){
+    function get_estancias_by_interval($date1, $date2){//devolverá las estancias entre dos fechas
         $sql = "SELECT * FROM `reservas` WHERE fecha_entrada <'".$date1."' AND fecha_salida > '".$date2."'";
         $enlace = conectar();
         $result = $enlace->query($sql);
@@ -44,6 +44,14 @@
         return $rows;
     }
    
-    
+    function get_persons_reserved($month='m'){//devolverá la suma de personas que han entrado cada mes agrupadas por mes
+        $date = date("Y-".$month."-01");  
+        $sql = "SELECT SUM(personas) as 'Total Personas', MONTH(fecha_entrada) as 'Mes Entrada' FROM reservas WHERE fecha_entrada > '".$date."' GROUP BY MONTH(fecha_entrada)";
+        $enlace = conectar();
+        $result = $enlace->query($sql);
+        $rows=$result->fetch_all(MYSQLI_ASSOC);
+        return $rows;
+    }
+
    
 ?>      
